7a6739c49196611e03438d68974881f1dfe6c566
Rahul Tummala <rahul@Rahuls-MBP.attlocal.net>
2024-05-01 22:59:29 -0400

    first commit

---
+++ helm-jenkins.sh
@@ -0,0 +1,13 @@
+helm repo add jenkins https://charts.jenkins.io
+helm repo update
+helm upgrade --install myjenkins jenkins/jenkins
+
+# kubectl exec --namespace default -it svc/myjenkins -c jenkins -- /bin/cat /run/secrets/additional/chart-admin-password && echo
+kubectl --namespace default port-forward svc/myjenkins 8080:8080
+
+# Helm - show values jenkins/jenkins
+helm upgrade --install -f values.yaml myjenkins jenkins/jenkins
+
+# Get jenkins URL
+# kubectl get svc --namespace default myjenkins --template "{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}"
+# kubectl exec --namespace default -it svc/myjenkins -c jenkins -- /bin/cat /run/secrets/additional/chart-admin-password && echo
